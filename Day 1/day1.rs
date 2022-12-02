6e305182b315a582e57eea9160b5f03fdfae83ed
manu <manuel.adalidmoya@jobtome.com>
2022-12-02 10:36:56 +0100

    push

--- Day 1/day1.rs
+++ Day 1/day1.rs
@@ -4,21 +4,32 @@ fn main() {
     let input = fs::read_to_string("input.txt").
         expect("Should have been able to read the input");
 
-    let mut highest = 0;
+    let mut first = 0;
+    let mut second = 0;
+    let mut third = 0;
     let mut current = 0;
     for line in input.split("\n") {
         match line {
             "" => {
-                highest = if current > highest { current } else { highest };
+                (first, second, third) = get_top_three(current, first, second, third);
                 current = 0;
             },
             _ => current += line.parse::<i32>().unwrap(),
         }
     }
 
-    println!("{highest}");
+    println!("{first}");
+    println!("{}", first+second+third);
 }
 
-fn getTopThree(current, first, second, third: i32) -> (i32, i32, i32) {
-    
+fn get_top_three(current: i32, first: i32, second: i32, third: i32) -> (i32, i32, i32) {
+   if current > first {
+       return (current, first, second)
+   } else if current > second {
+       return (first, current, second)
+   } else if current > third {
+       return (first, second, current)
+   }
+
+   return (first, second, third)
 }
