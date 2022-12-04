287411eb9e5e036da1363ed768ce7f7d3d8704ee
manu <manuel.adalidmoya@jobtome.com>
2022-12-04 11:29:15 +0100

    day 4 part 2

--- Day 4/day4.rs
+++ Day 4/day4.rs
@@ -10,13 +10,45 @@ fn main() {
     let input = fs::read_to_string("input.txt").expect("Should have been able to read the input");
 
     println!("{}", part_one(input.clone()));
+    println!("{}", part_two(input.clone()));
 }
 
 fn part_one(input: String) -> u32 {
     let mut fully_contained = 0;
 
     for pair in input.split("\n") {
-        let pair: Vec<&str> = pair.split(",").collect();
+        let (first, second) = get_pairs(pair);
+
+        if (first.start <= second.start && first.end >= second.end)
+            || (second.start <= first.start && second.end >= first.end) {
+            fully_contained+=1;
+        }
+    }
+
+    return fully_contained;
+}
+
+fn part_two(input: String) -> u32 {
+    let mut overlap = 0;
+
+    for pair in input.split("\n") {
+        let (first, second) = get_pairs(pair);
+
+        if first.start == second.start || first.start == second.end
+            || first.end == second.start || first.end == second.end {
+            overlap+=1;
+        } else if first.start < second.start && first.end >= second.start {
+            overlap+=1;
+        } else if first.start > second.start && first.start <= second.end {
+            overlap+=1;
+        }
+    }
+
+    return overlap;
+}
+
+fn get_pairs(pair: &str) -> (Range, Range) {
+    let pair: Vec<&str> = pair.split(",").collect();
 
         let (start, end) = get_range_start_end(pair[0]);
         let first = Range {
@@ -31,13 +63,7 @@ fn part_one(input: String) -> u32 {
             end: end,
         };
 
-        if (first.start <= second.start && first.end >= second.end)
-            || (second.start <= first.start && second.end >= first.end) {
-            fully_contained+=1;
-        }
-    }
-
-    return fully_contained;
+        return (first, second);
 }
 
 fn get_range_start_end(range: &str) -> (i32, i32) {
