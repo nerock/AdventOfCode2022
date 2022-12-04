9aacc9c91cf550954f7f9895cb0d3a66cac1b9e2
manu <manuel.adalidmoya@jobtome.com>
2022-12-04 20:33:47 +0100

    separate part 1 and two

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,22 +3,48 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("input.txt").expect("Should have been able to read the input");
 
+    println!("{}", part_one(input.clone()));
+    let (first, second, third) = part_two(input.clone());
+    println!("{} {} {}", first, second, third);
+}
+
+fn part_one(input: String) -> i32 {
+    let mut most_calories = 0;
+    let mut current = 0;
+    for calories in input.split("\n") {
+        match calories {
+            "" => {
+                most_calories = if current > most_calories { current } else { most_calories };
+                current = 0;
+            }
+            _ => current += get_calories(calories),
+        }
+    }
+
+    return most_calories;
+}
+
+fn part_two(input: String) -> (i32, i32, i32) {
     let mut first = 0;
     let mut second = 0;
     let mut third = 0;
     let mut current = 0;
-    for line in input.split("\n") {
-        match line {
+
+    for calories in input.split("\n") {
+        match calories {
             "" => {
                 (first, second, third) = get_top_three(current, first, second, third);
                 current = 0;
             }
-            _ => current += line.parse::<i32>().unwrap(),
+            _ => current += get_calories(calories),
         }
     }
 
-    println!("{first}");
-    println!("{}", first + second + third);
+    return (first, second, third);
+}
+
+fn get_calories(calories: &str) -> i32 {
+    return calories.parse().expect("should be a number")
 }
 
 fn get_top_three(current: i32, first: i32, second: i32, third: i32) -> (i32, i32, i32) {
